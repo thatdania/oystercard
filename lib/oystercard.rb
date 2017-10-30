6ec9c41f25a4e5e4f65ec2ff31c9152a2be89f76
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-10-30 17:59:31 +0000

    step 6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -10,15 +10,11 @@ DEFAULT_BALANCE = 0
   end
 
 def top_up(value)
-<<<<<<< HEAD
   if @balance > 90 || @balance + value > 90
     raise 'Oystercard has reached the limit'
   else
     @balance = @balance + value
   end
-=======
-  5
->>>>>>> c99dd74e31e668f212f4c5d08c91c05af57eb33b
 end
 
 # def balance(value = DEFAULT_BALANCE)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,7 +11,6 @@ describe Oystercard do
 
   describe "#top_up" do
     it 'Adds money to card' do
-<<<<<<< HEAD
     expect{subject.top_up(1)}.to change{subject.balance }.by(1)
     end
 
@@ -21,12 +20,6 @@ describe Oystercard do
       card = Oystercard.new 90
       expect{card.top_up(1)}.to raise_error 'Oystercard has reached the limit'
     end
-=======
-    expect(subject.top_up("value")).to eq 5
-    end
-
-    it {is_expected.to respond_to{:top_up}.with(1).argument}
->>>>>>> c99dd74e31e668f212f4c5d08c91c05af57eb33b
 
   end
 end
