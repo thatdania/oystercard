c99dd74e31e668f212f4c5d08c91c05af57eb33b
thatdania@gmail.com <thatdania@gmail.com>
2017-10-30 17:00:06 +0000

    added two tests for top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,7 +7,14 @@ describe Oystercard do
       it 'Sets new card to have a default balance of 0' do
         expect(subject.balance).to eq 0
       end
+    end
 
+  describe "#top_up" do
+    it 'Adds money to card' do
+    expect(subject.top_up("value")).to eq 5
+    end
+
+    it {is_expected.to respond_to{:top_up}.with(1).argument}
 
   end
 end
