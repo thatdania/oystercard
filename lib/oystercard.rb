b921a8bd98b58b076289d8141b749b54ccffc7ee
thatdania@gmail.com <thatdania@gmail.com>
2017-10-30 16:59:37 +0000

    added top up method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,16 @@
 
 class Oystercard
 
-attr_reader :balance
+attr_accessor :balance
 
 DEFAULT_BALANCE = 0
 
-def initialize (balance = DEFAULT_BALANCE)
- @balance = balance
+  def initialize (balance = DEFAULT_BALANCE)
+   @balance = balance
+  end
+
+def top_up(value)
+  5
 end
 
 # def balance(value = DEFAULT_BALANCE)
