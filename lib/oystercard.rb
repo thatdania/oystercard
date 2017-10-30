869dcb7704c35dfd0a4205d36c8772eadf013469
thatdania@gmail.com <thatdania@gmail.com>
2017-10-30 16:20:39 +0000

    Refactor to pass the added spec test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,17 @@
 
 class Oystercard
 
+attr_reader :balance
+
+DEFAULT_BALANCE = 0
+
+def initialize (balance = DEFAULT_BALANCE)
+ @balance = balance
+end
+
+# def balance(value = DEFAULT_BALANCE)
+#   @value
+# end
+
+
 end
